afc69748be7a69c1136d2467e47236214cca41dc
lc <lc_3lc@163.com>
2016-05-09 10:55:11 +0800

    new file

---
+++ EventUtil.js
@@ -0,0 +1,118 @@
+//http://www.cnblogs.com/chemandy/archive/2011/09/05/2168119.html
+var EventUtil = {
+
+    addHandler: function(element, type, handler){
+        if (element.addEventListener){
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent){
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
+        }
+    },
+   //□DOM中的button属性取值：0表示主鼠标按键,1鼠标中键（滚轮按钮）,2鼠标次键
+    //IE提供的button属性,0没按键◇1按主键◇2按次键◇3同时主+次◇4中键◇5主+中◇6次+中◇7主+次+中
+    //□mousedown和mouseup事件，在其event对象中有一button属性表示按下或释放的按钮。
+    //□如果不是按下或释放主键，opera不会触发mouseup或mousedown
+    getButton: function(event){
+        if (document.implementation.hasFeature("MouseEvents", "2.0")){
+            return event.button;
+        } else {
+            switch(event.button){
+                case 0:
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                    return 0;
+                case 2:
+                case 6:
+                    return 2;
+                case 4: return 1;
+            }
+        }
+    },
+
+    //□Firefox、Chrome和Safari的event对象支持一个charCode属性，这个属性只有在发生keypress时才有值，为字符的ASCII编码。
+    // 此时的keyCode值有可能为0，也可能为所按键码。IE和Opera则是在keyCode中保存ASCII编码。
+    //跨浏览器取字符编码
+    getCharCode: function(event){
+        if (typeof event.charCode == "number"){
+            return event.charCode;
+        } else {
+            return event.keyCode;
+        }
+    },
+    
+    getClipboardText: function(event){
+        var clipboardData =  (event.clipboardData || window.clipboardData);
+        return clipboardData.getData("text");
+    },
+    
+    getEvent: function(event){
+        return event ? event : window.event;
+    },
+    
+    getRelatedTarget: function(event){
+        if (event.relatedTarget){
+            return event.relatedTarget;
+        } else if (event.toElement){
+            return event.toElement;
+        } else if (event.fromElement){
+            return event.fromElement;
+        } else {
+            return null;
+        }
+    
+    },
+    
+    getTarget: function(event){
+        return event.target || event.srcElement;
+    },
+    
+    getWheelDelta: function(event){
+        if (event.wheelDelta){
+            return (client.engine.opera && client.engine.opera < 9.5 ? -event.wheelDelta : event.wheelDelta);
+        } else {
+            return -event.detail * 40;
+        }
+    },
+    
+    //取消事件默认行为
+    preventDefault: function(event){
+        if (event.preventDefault){
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    },
+
+    removeHandler: function(element, type, handler){
+        if (element.removeEventListener){
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent){
+            element.detachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = null;
+        }
+    },
+    
+    setClipboardText: function(event, value){
+        if (event.clipboardData){
+            event.clipboardData.setData("text/plain", value);
+        } else if (window.clipboardData){
+            window.clipboardData.setData("text", value);
+        }
+    },
+
+
+    //取消冒泡
+    stopPropagation: function(event){
+        if (event.stopPropagation){
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    }
+
+};
